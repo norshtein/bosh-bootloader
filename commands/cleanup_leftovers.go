d44b6c15569b2b794a10c41a442ae30e22bca07b
Connor Braa <connor.braa@gmail.com>
2018-02-01 17:50:05 -0800

    WIP don't always collect leftovers

--- commands/cleanup_leftovers.go
+++ commands/cleanup_leftovers.go
@@ -5,19 +5,23 @@ import (
 
 	"github.com/cloudfoundry/bosh-bootloader/flags"
 	"github.com/cloudfoundry/bosh-bootloader/storage"
+
+	awsleftovers "github.com/genevievelesperance/leftovers/aws"
+	azureleftovers "github.com/genevievelesperance/leftovers/azure"
+	gcpleftovers "github.com/genevievelesperance/leftovers/gcp"
 )
 
-type FilteredDeleter interface {
-	Delete(filter string) error
-}
-
 type CleanupLeftovers struct {
-	deleter FilteredDeleter
+	logger      logger
+	iaas        string
+	credentials []string
 }
 
-func NewCleanupLeftovers(deleter FilteredDeleter) CleanupLeftovers {
+func NewCleanupLeftovers(logger logger, iaas string, credentials ...string) CleanupLeftovers {
 	return CleanupLeftovers{
-		deleter: deleter,
+		logger:      logger,
+		iaas:        iaas,
+		credentials: credentials,
 	}
 }
 
@@ -26,16 +30,35 @@ func (l CleanupLeftovers) CheckFastFails(subcommandFlags []string, state storage
 }
 
 func (l CleanupLeftovers) Execute(subcommandFlags []string, state storage.State) error {
+	var (
+		deleter interface {
+			Delete(string) error
+		}
+		err error
+	)
+
+	switch l.iaas {
+	case "aws":
+		deleter, err = awsleftovers.NewLeftovers(l.logger, l.credentials...)
+	case "azure":
+		deleter, err = azureleftovers.NewLeftovers(l.logger, l.credentials...)
+	case "gcp":
+		deleter, err = gcpleftovers.NewLeftovers(l.logger, l.credentials...)
+	}
+	if err != nil {
+		return fmt.Errorf("Collecting leftovers: %s", err)
+	}
+
 	var filter string
 	f := flags.New("cleanup-leftovers")
 	f.String(&filter, "filter", "")
 
-	err := f.Parse(subcommandFlags)
+	err = f.Parse(subcommandFlags)
 	if err != nil {
 		return fmt.Errorf("Parsing cleanup-leftovers args: %s", err)
 	}
 
-	return l.deleter.Delete(filter)
+	return deleter.Delete(filter)
 }
 
 func (l CleanupLeftovers) Usage() string {
