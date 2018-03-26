e086ab6138448515afa16bea3b024cc0fdcc2687
Tongyao Si <t-tosi@microsoft.com>
2018-03-26 07:25:23 +0000

    Working version

--- terraform/azure/templates/cf_dns.tf
+++ terraform/azure/templates/cf_dns.tf
@@ -5,7 +5,7 @@ data "azurerm_public_ip" "cf-lb" {
 }
 
 resource "azurerm_dns_zone" "cf" {
-  name                = "${var.env_id}.${var.system_domain}"
+  name                = "${var.system_domain}"
   resource_group_name = "${azurerm_resource_group.bosh.name}"
 }
 
